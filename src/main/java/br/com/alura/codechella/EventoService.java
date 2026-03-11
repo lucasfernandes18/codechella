8049f50ae5eda91cefc58788f01426d26a32a559
Seu Nome <lucasfermagalhaes@gmail.com>
2026-03-10 22:13:22 -0300

    feat: criado o método de obter por id com tratamento de exceção

--- src/main/java/br/com/alura/codechella/EventoService.java
+++ src/main/java/br/com/alura/codechella/EventoService.java
@@ -1,8 +1,12 @@
 package br.com.alura.codechella;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -11,6 +15,13 @@ public class EventoService {
 
     public Flux<EventoDTO> obterTodos() {
         return eventoRepository.findAll()
+                .map(EventoDTO::toDTO);
+    }
+
+    public Mono<EventoDTO> obterPorId(Long id) {
+        return eventoRepository.findById(id)
+                //se recebido um id que não existe lançará a exceção.
+                .switchIfEmpty( Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .map(EventoDTO::toDTO);
     }
 
